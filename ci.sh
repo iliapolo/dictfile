75220d3799a5ba47d80a38ce7f78373228f50663
iliapolo <eli.polonsky@gmail.com>
2018-04-06 18:39:08 +0300

    added message

--- ci.sh
+++ ci.sh
@@ -17,6 +17,7 @@ echo "Running tests"
 py.test --cov-report term-missing --cov=fileconfig fileconfig/tests
 
 if [ -z ${TRAVIS_PULL_REQUEST_BRANCH} ] && [ "dev" = ${TRAVIS_BRANCH} ]; then
+    echo "Running release"
     pip install https://github.com/iliapolo/pyrelease/archive/master.zip
     pyrelease release --repo iliapolo/fileconfig --branch dev
 fi
